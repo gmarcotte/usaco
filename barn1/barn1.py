d5fad9b07d7f961ae52db2a35cba0a7b9be77b3c
Garrett Marcotte <garrett.marcotte@gmail.com>
2019-10-16 00:08:39 -0700

    start of barn1

---
+++ barn1/barn1.py
@@ -0,0 +1,13 @@
+"""
+ID: garrett2
+TASK: barn1
+LANG: PYTHON3
+"""
+
+def main():
+  fin = open('barn1.in', 'r')
+  (m, s, c) = (int(s) for s in fin.readline().strip().split(' '))
+
+
+if __name__ == '__main__':
+  main()
